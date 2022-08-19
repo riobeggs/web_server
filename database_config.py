6f79edfb82fbd9dfc3e9ddaea014e19d3a55c80d
Rio <riobeggs1@gmail.com>
2022-08-19 14:26:46 +1200

    added database config file

---
+++ database_config.py
@@ -0,0 +1,33 @@
+import os
+import psycopg2
+import json
+
+os.system("createdb webserver")
+
+config_file = open(
+    # replace user specific config.json path
+    "/Users/riobeggs/Documents/code/web_server/webserver_files/config.json"
+)
+
+json = json.load(config_file)
+
+conn = psycopg2.connect(
+    user=json["user"],
+    password=json["password"],
+    host=json["host"],
+    port=json["port"],
+    dbname=json["dbname"],
+)
+
+conn.autocommit = True
+cursor = conn.cursor()
+
+create_table = """CREATE TABLE PEOPLE(
+    ID INT PRIMARY KEY NOT NULL,
+    FIRST_NAME VARCHAR(50),
+    LAST_NAME VARCHAR(50),
+    DOB DATE
+    )"""
+
+cursor.execute(create_table)
+conn.close()
